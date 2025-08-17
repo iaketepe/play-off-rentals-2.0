e6ebd28485378bd5c279b170b8d5d288bb016c5e
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2025-08-17 19:55:10 -0400

    Updated app.jsx name to root for readability. Removed uncessary fonts

--- resources/js/root.jsx
+++ resources/js/root.jsx
@@ -2,8 +2,8 @@ import './bootstrap';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function App() {
+function Root() {
     return <h1>Hello from React!</h1>;
 }
 
-ReactDOM.createRoot(document.getElementById('react-root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('react-root')).render(<Root />);
