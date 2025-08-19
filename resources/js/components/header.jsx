5bf44b796a5f5e2140160b1fd606293a5c66d308
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2025-08-19 14:10:06 -0400

    Added Font Awesome link for icons. Updated body to use h-screen as its default height. Updated Header nav with icons, spacing, etc

--- resources/js/components/header.jsx
+++ resources/js/components/header.jsx
@@ -1,11 +1,17 @@
 
 function Header() {
     return (
-        <nav>
-            <i className="fa-solid hamburger"></i>
-            <i className="fa-solid fa-gamepad"></i>
-            <button>EN</button>
-            <button>Log In/Out</button>
+        <nav className="bg-slate-500 p-5 flex items-center">
+            <div className="flex flex-1">
+                <i className="fa-solid fa-bars"></i>
+            </div>
+
+            <i className="fa-solid fa-gamepad text-3xl"></i>
+            
+            <div className="flex flex-1 justify-end space-x-3 ">
+                <button className="text-md">Connexion</button>
+            </div>
+            
         </nav>
     );
 }
