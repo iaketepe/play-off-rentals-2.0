94221eaba8739dabe90ca8fc72b653beb1516ab9
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2025-08-21 13:20:53 -0400

    Created Overview.md. Converted Nav elements to anchors, added styling to Log In button

--- resources/js/components/header.jsx
+++ resources/js/components/header.jsx
@@ -1,17 +1,16 @@
 
 function Header() {
     return (
-        <nav className="bg-slate-500 p-5 flex items-center">
+        <nav className="bg-slate-500 p-5 px-4 flex items-center">
             <div className="flex flex-1">
-                <i className="fa-solid fa-bars"></i>
+                <a href="#"><i className="fa-solid fa-bars"></i></a>
             </div>
 
-            <i className="fa-solid fa-gamepad text-3xl"></i>
+            <a href="#"><i className="fa-solid fa-gamepad text-3xl"></i></a>
             
             <div className="flex flex-1 justify-end space-x-3 ">
-                <button className="text-md">Connexion</button>
+                <a className="bg-gray-700 text-sm border-black border-2 p-4 py-2 rounded-full" href="#">Connexion</a>
             </div>
-            
         </nav>
     );
 }
