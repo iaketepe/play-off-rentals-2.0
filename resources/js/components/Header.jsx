9031aa9ec3a3589808fc0691ff3f54c3d4ecb788
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2025-08-24 19:04:39 -0400

    Added Header.jsx file to verify case sensitvity issue has been fixed

---
+++ resources/js/components/Header.jsx
@@ -0,0 +1,23 @@
+
+function Header() {
+    return (
+        <nav className="bg-slate-500 flex flex-col border-red-700 border-2">
+            <div className="flex p-5 pr-3 items-center justify-between">
+                <div className="">
+                    <button><i className="fa-solid fa-bars"></i></button>
+                </div>
+
+                <a href="#" className=""><i className="fa-solid fa-gamepad text-3xl"></i></a>
+                
+                <div className="">
+                    <a className="bg-gray-700 text-sm border-black border-2 p-4 py-2 rounded-full" href="#">Connexion</a>
+                </div>
+            </div>
+
+
+        </nav>
+    );
+}
+
+
+export default Header;
