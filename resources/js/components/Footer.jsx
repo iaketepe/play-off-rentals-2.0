3d190c4265d031b7c0a0d2cc4715c4db321d0806
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2025-08-27 11:02:14 -0400

    Aligned Hours of Ooperation element

--- resources/js/components/Footer.jsx
+++ resources/js/components/Footer.jsx
@@ -7,48 +7,48 @@ function Footer() {
                     <h5>Location Details</h5>
                 </div>
                 <div className="text-black basis-1/3 bg-white p-5 border-green-700 border-2 rounded-xl">
-                    <h5>Hours of Operation</h5>
-                    <table>
+                    <h5 className="text-lg font-semibold text-center">Hours of Operation</h5>
+                    <table className="w-full">
                         <thead>
-                            <th>Day</th>
-                            <th>Open</th>
-                            <th>Closed</th>
+                            <th className="text-left">Day</th>
+                            <th className="text-right">Open</th>
+                            <th className="text-right">Closed</th>
                         </thead>
                         <tbody>
                             <tr>
                                 <td>Monday</td>
-                                <td>9am</td>
-                                <td>5pm</td>
+                                <td className="text-right">9am</td>
+                                <td className="text-right">5pm</td>
                             </tr>
                             <tr>
                                 <td>Tuesday</td>
-                                <td>9am</td>
-                                <td>5pm</td>
+                                <td className="text-right">9am</td>
+                                <td className="text-right">5pm</td>
                             </tr>
                             <tr>
                                 <td>Wednesday</td>
-                                <td>N/A</td>
-                                <td>N/A</td>
+                                <td className="text-right">N/A</td>
+                                <td className="text-right">N/A</td>
                             </tr>
                             <tr>
                                 <td>Thursday</td>
-                                <td>N/A</td>
-                                <td>N/A</td>
+                                <td className="text-right">N/A</td>
+                                <td className="text-right">N/A</td>
                             </tr>
                             <tr>
                                 <td>Friday</td>
-                                <td>9am</td>
-                                <td>5pm</td>
+                                <td className="text-right">9am</td>
+                                <td className="text-right">5pm</td>
                             </tr>
                             <tr>
                                 <td>Saturday</td>
-                                <td>9am</td>
-                                <td>5pm</td>
+                                <td className="text-right">9am</td>
+                                <td className="text-right">5pm</td>
                             </tr>
                             <tr>
                                 <td>Sunday</td>
-                                <td>9am</td>
-                                <td>5pm</td>
+                                <td className="text-right">9am</td>
+                                <td className="text-right">5pm</td>
                             </tr>
                         </tbody>
                     </table>
