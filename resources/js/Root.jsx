6f35416ad285d67dc4230c62a203cf1b083a2407
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2025-08-31 15:07:42 -0400

    Restructred Project to allow for laravel routing. Renamed home.blade.php to app.blade.php for consistency. Added RentCore.

--- resources/js/Root.jsx
+++ resources/js/Root.jsx
@@ -3,15 +3,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './components/Header';
 import HomeCore from './cores/HomeCore';
+import RentCore from './cores/RentCore';
 import Footer from './components/Footer';
 
 import "./i18n";
 
-function Root() {
+//const rootDiv = document.getElementById('react-root');
+//const page = rootDiv ? rootDiv.dataset.page : 'home';
+
+function Root({page}) {
+
+    const cores = {
+        home: <HomeCore/>,
+        rent: <RentCore/>,
+        //contact: <ContactCore/>
+    };
+
+    const Core = cores[page] || cores['home'];
+
     return (
         <div className='flex flex-col h-full'>
             <Header/>
-            <HomeCore/>
+            {Core}
             <Footer/>
         </div>
     );
---
+++ resources/js/cores/RentCore.jsx
@@ -0,0 +1,12 @@
+
+
+function RentCore () {
+    return (
+        <div>
+            <h1>RentCore</h1>
+        </div>
+    );
+
+}
+
+export default RentCore;
