ecad574e2b4a4037f1bdb24810a8824e1a1e00d9
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2025-09-07 17:11:51 -0400

    Updated RentOne markup into three subelements

--- resources/js/components/RentOne.jsx
+++ resources/js/components/RentOne.jsx
@@ -3,8 +3,18 @@
 
 function RentOne() {
     return(
-        <div><h1 className='text-black text-4xl font-semibold'>Confirm Your Location</h1>
-        <h3>(Click Next to confirm later...)</h3>
+        <div className="w-full flex flex-col divide-y divide-black gap-5">
+            <div className="space-x-5">
+                <h1 className='inline text-black text-4xl font-semibold'>Confirm Your Location</h1>
+                <span className="text-xl">(Click Next to confirm later...)</span>
+            </div>
+            <div className="flex flex-col flex-1 gap-5 divide-y divide-black">
+                <input className="border-black border-2 p-3 rounded-lg max-w-" type="search" name="" id="" placeholder="Type to Search" />
+                <div>
+                    Geolocation
+                </div>
+            </div>
+
         </div>
 
     )
