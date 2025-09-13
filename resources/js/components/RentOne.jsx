6bd6fd1a039d87536fbb8ba2ba6f95735a247690
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2025-09-13 14:34:46 -0400

    Reorganized Core components in relation to flex-1 wrapping from last commit. Installed leaflet for Geo step in Rent process

--- resources/js/components/RentOne.jsx
+++ resources/js/components/RentOne.jsx
@@ -3,14 +3,14 @@
 
 function RentOne() {
     return(
-        <div className="w-full flex flex-col divide-y divide-black gap-5">
+        <div className="w-full h-full flex flex-col divide-y divide-black gap-10 border-black border-2">
             <div className="space-x-5">
                 <h1 className='inline text-black text-4xl font-semibold'>Confirm Your Location</h1>
                 <span className="text-xl">(Click Next to confirm later...)</span>
             </div>
             <div className="flex flex-col flex-1 gap-5 divide-y divide-black">
                 <input className="border-black border-2 p-3 rounded-lg max-w-" type="search" name="" id="" placeholder="Type to Search" />
-                <div>
+                <div className="border-black border-2 m-auto w-full max-w-lg h-[20rem] rounded-lg">
                     Geolocation
                 </div>
             </div>
