7f42fba1aea028a86d86e4af37dc8ac960a7b3fe
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2025-09-15 17:52:46 -0400

    Rendered map tiles using leaflet

--- resources/js/components/RentOne.jsx
+++ resources/js/components/RentOne.jsx
@@ -1,9 +1,22 @@
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import { useEffect } from "react";
 
 
 
 function RentOne() {
+    useEffect(() => {
+        var map = L.map('map').setView([51.505, -0.09], 13);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+        //mapDiv.innerHTML = map;
+    });
+    
+
+
     return(
         <div className="w-full h-full flex flex-col divide-y divide-black gap-10 border-black border-2">
             <div className="space-x-5">
@@ -12,8 +25,8 @@ function RentOne() {
             </div>
             <div className="flex flex-col flex-1 gap-5 divide-y divide-black">
                 <input className="border-black border-2 p-3 rounded-lg max-w-" type="search" name="" id="" placeholder="Type to Search" />
-                <div className="border-black border-2 m-auto w-full max-w-lg h-[20rem] rounded-lg">
-                    Geolocation
+                <div id="map" className="border-black border-2 m-auto w-full max-w-lg h-[20rem] rounded-lg">
+                    
                 </div>
             </div>
 
