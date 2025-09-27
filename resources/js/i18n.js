1011c98636ce6032ab6dd9a4807571198ba48e98
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2025-09-27 15:53:32 -0400

    Updated i18n to change en/fr locale based on user. Updated i18n, allowing translations to persist over full reloads

--- resources/js/i18n.js
+++ resources/js/i18n.js
@@ -1,5 +1,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 import en from "../locales/en.json";
 import fr from "../locales/fr.json";
 
@@ -12,13 +13,20 @@ const resources = {
 };
 
 i18n
+  .use(LanguageDetector)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: "en",
     fallbackLng: "en",
     interpolation: {
       escapeValue: false // react already safes from xss
+    },
+    detection:{
+      order: ["cookie", "navigator"],
+      caches: ["cookie"],
+      lookupCookie: "i18nextLng",
+      cookieOptions: { path: "/", secure: true, sameSite: "lax" },
+      load: "languageOnly",
     }
   });
 
