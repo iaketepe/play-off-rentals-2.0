d3b7d1840a5c2ea1a4df305340d822fae6b253f2
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2025-10-03 21:23:29 -0400

    Beginning to integrate DB to web store

---
+++ app/Http/Controllers/DataController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Services\DbObject;
+
+class DataController extends Controller {
+    private DbObject $dbObject;
+}
