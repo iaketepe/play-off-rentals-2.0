2d2d4f9d6048aee6b6e12f44fa1e6de48aebe25b
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2025-10-19 23:24:12 -0400

    Integrated ArcadeCard component into RentTwo. Added cart functionality

--- resources/js/components/RentTwo.jsx
+++ resources/js/components/RentTwo.jsx
@@ -1,11 +1,10 @@
 import { useTranslation } from "react-i18next";
 import { useEffect, useState } from "react";
+import ArcadeCard from "./ArcadeCard";
 
 function RentTwo() {
     const { t } = useTranslation();
     
-    
-    const elem = ['Test Arcade','Test Description','50/Day'];
     const test = [
         { id: 1, title: "Card One", description: "Navigate the maze, munch on dots, and outsmart the ghosts in this timeless arcade classic by Namco!", price: "50/day" },
         { id: 2, title: "Card Two", description: "Navigate the maze, munch on dots, and outsmart the ghosts in this timeless arcade classic by Namco!", price: "75/day" },
@@ -47,20 +46,12 @@ function RentTwo() {
         <div className="w-full flex flex-col gap-10">
             <div className="flex flex-col gap-10">
                 <h1 className='inline text-black text-4xl font-semibold'>{t("rentTwo.title")}</h1>
-                <div id="arcadeList" className="m-auto grid grid-cols-1 md:grid-cols-3 gap-4">
+                <div id="arcadeList" className="grid grid-cols-1 [@media(min-width:500px)_and_(max-width:767px)]:grid-cols-2 md:grid-cols-3 gap-4">
                     {machines.map((item, i) => (
-                        <div
-                        key={i}
-                        className="rounded-lg p-2 py-3 bg-slate-500"
-                        loading="lazy"
-                        >
-                            <img src={item.image_path} className="bg-black w-full h-[262px] rounded-md" alt={item.name} />
-                            <div className="p-3">
-                                <h2 className="text-xl font-bold">{item.name}</h2>
-                                <p>{item.description}</p>
-                                <p>{item.cost}/Day</p>
-                            </div>
-                        </div>
+                        <ArcadeCard
+                            key={i}
+                            item={item}
+                        />
                     ))}
                 </div>
             </div>
