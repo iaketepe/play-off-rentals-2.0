b37c2e7e5cff9b80eb6c08fb2fd125b6e1de67f5
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2025-10-21 00:19:31 -0400

    Updating defineConfig

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,19 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ _, mode }) => ({
     plugins: [
         laravel({
             input: ['resources/css/Root.css', 'resources/js/Root.jsx'],
             refresh: true,
         }),
-        tailwindcss(), react()
+        tailwindcss(),
+        react(),
     ],
     base: '/build/',
     server: {
-        origin: mode === 'production' ? 'https://play-off-rentals-2-0.onrender.com' : 'http://localhost:5173',
+        origin: mode === 'production'
+            ? 'https://play-off-rentals-2-0.onrender.com'
+            : 'http://localhost:5173',
     },
-});
+}));
