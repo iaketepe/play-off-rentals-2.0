8a279f7fed46361704049fa0f80b0eacff7633c5
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2025-10-28 14:29:44 -0400

    Translated payment form submit button

--- resources/js/components/PaymentForm.jsx
+++ resources/js/components/PaymentForm.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { useTranslation } from "react-i18next";
 import { PaymentElement, useStripe, useElements } from '@stripe/react-stripe-js';
 
 function PaymentForm() {
+  const { t } = useTranslation();
   const stripe = useStripe();
   const elements = useElements();
 
@@ -30,7 +32,7 @@ function PaymentForm() {
   return (
     <form onSubmit={handleSubmit} className='h-full flex flex-col justify-between'>
       <PaymentElement options={{ layout : 'tabs' }} />
-      <input type="submit" className='text-center w-full p-3 py-2 border-2 border-black rounded-full cursor-pointer transition-colors duration-300 ease-in-out hover:bg-gray-600 hover:text-white' disabled={!stripe} value="Submit"/>
+      <input type="submit" className='text-center w-full p-3 py-2 border-2 border-black rounded-full cursor-pointer transition-colors duration-300 ease-in-out hover:bg-gray-600 hover:text-white' disabled={!stripe} value={t("paymentForm.submit")}/>
     </form>
   )
 
