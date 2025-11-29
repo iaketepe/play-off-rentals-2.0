a52a62c7e9dc65b4a2e1d00b28eacda7edd559c2
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2025-11-29 17:09:39 -0500

    Converted GmailService from SMTP to HTTP Request to deal with Render blocking outbound SMTP

--- app/Services/GmailService.php
+++ app/Services/GmailService.php
@@ -3,11 +3,12 @@
 namespace App\Services;
 
 use Illuminate\Support\Facades\View;
-use Symfony\Component\Mailer\Mailer;
+//use Symfony\Component\Mailer\Mailer;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Mailer\Transport\Smtp\EsmtpTransport;
 use Symfony\Component\Mailer\Transport\Smtp\Auth\Xoauth2Authenticator;
 use App\Services\GmailOauthService;
+use Illuminate\Support\Facades\Http;
 
 
 class GmailService {
@@ -26,7 +27,7 @@ class GmailService {
 
 
         $content = $mailable->content();
-        $mailer = new Mailer($transport);
+        //$mailer = new Mailer($transport);
 
         $plain = View::make($content->text, $content->with)->render();
         $html = View::make($content->view, $content->with)->render();
@@ -34,6 +35,7 @@ class GmailService {
         $mailableFrom = $mailable->envelope()->from;
         $from = "{$mailableFrom->name} <{$mailableFrom->address}>";
 
+        //Create a Symfony email object
         $email = new Email();
 
         $email->from($from ?? env('MAIL_FROM_ADDRESS'))
@@ -41,7 +43,23 @@ class GmailService {
             ->subject($mailable->envelope()->subject)
             ->text($plain)
             ->html($html);
-        $mailer->send($email);
+        //$mailer->send($email);
+
+        //Send object as a MIME via HTTPS
+        $rawMime = $email->toString();
+
+        $rawEncoded = rtrim(strtr(base64_encode($rawMime), '+/', '-_'), '=');
+
+        $response = Http::withHeaders([
+            'Authorization' => "Bearer {$accessToken}",
+            'Content-Type' => 'application/json',
+        ])->post('https://gmail.googleapis.com/gmail/v1/users/me/messages/send', [
+            'raw' => $rawEncoded,
+        ]);
+
+        if ($response->failed()) {
+            throw new \Exception("Gmail API send failed.");
+        }
     }
 
 }
