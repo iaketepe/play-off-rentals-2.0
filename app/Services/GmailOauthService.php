3f80be623bb1a4f24ef73547b2edc654741c21c2
Iyanu Aketepe <55199789+iyan5440@users.noreply.github.com>
2025-11-28 20:42:26 -0500

    Updated GmailService for flexible CI/CD

--- app/Services/GmailOauthService.php
+++ app/Services/GmailOauthService.php
@@ -13,8 +13,14 @@ class GmailOauthService
             'clientSecret' => env('GOOGLE_CLIENT_SECRET'),
         ]);
 
+        $refreshToken = env('GOOGLE_REFRESH_TOKEN');
+
+        if(env('APP_ENV') !== 'local') {
+            $refreshToken = env('GOOGLE_REFRESH_LOCAL_TOKEN');
+        }
+
         $token = $provider->getAccessToken('refresh_token', [
-            'refresh_token' => env('GOOGLE_REFRESH_TOKEN'),
+            'refresh_token' => $refreshToken,
         ]);
 
         return $token->getToken();
